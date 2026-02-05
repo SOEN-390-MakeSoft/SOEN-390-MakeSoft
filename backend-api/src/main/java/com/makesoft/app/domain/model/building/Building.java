f0a2f5525a2dbefb7813ad1658f477edcbcf8f64
Ryan Cheung <ryanch246@gmail.com>
2026-02-04 22:27:44 -0500

    Refactor Building class to make fields final and remove setters

--- backend-api/src/main/java/com/makesoft/app/domain/model/building/Building.java
+++ backend-api/src/main/java/com/makesoft/app/domain/model/building/Building.java
@@ -2,11 +2,11 @@ package com.makesoft.app.domain.model.building;
 
 public class Building {
     private final BuildingId buildingId;
-    private String name;
-    private String code;
-    private String address;
-    private String campus;
-    private BuildingFeatures buildingFeatures;
+    private final String name;
+    private final String code;
+    private final String address;
+    private final String campus;
+    private final BuildingFeatures buildingFeatures;
 
     public Building(BuildingId buildingId, String name, String code, String address, String campus,
                     BuildingFeatures buildingFeatures) {
@@ -26,39 +26,19 @@ public class Building {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getCode() {
         return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
     }
 
     public String getAddress() {
         return address;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     public String getCampus() {
         return campus;
     }
 
-    public void setCampus(String campus) {
-        this.campus = campus;
-    }
-
     public BuildingFeatures getBuildingFeatures() {
         return buildingFeatures;
-    }
-
-    public void setBuildingFeatures(BuildingFeatures buildingFeatures) {
-        this.buildingFeatures = buildingFeatures;
     }
 }
